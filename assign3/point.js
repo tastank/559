da04799964de3adadcebe773dc6c12642f7132d0
Tyler A Stank <tbear2500@gmail.com>
2015-09-23 22:49:44 -0500

    Initial commit

---
+++ assign3/point.js
@@ -0,0 +1,18 @@
+class point {
+    var x;
+    var y;
+    var z;
+
+    constructor(x,y,z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    function set(x,y,z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+}
