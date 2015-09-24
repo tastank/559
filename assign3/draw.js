021f92be8b40f2f904d412f1cfdd0a5b551953da
Tyler A Stank <tbear2500@gmail.com>
2015-09-24 12:57:50 -0500

    Updated to reflect changes in Point/Rprism; now includes prototype 2d transformation functions

--- assign3/draw.js
+++ assign3/draw.js
@@ -1,11 +1,24 @@
 "use strict"
 
-
-var line = function(p1, p2) {
-}
-
 var canvas = document.getElementById('main_canvas');
 var ctx = canvas.getContext('2d');
+
+//flattens points into two dimensions
+var get2d_coords = function(point, camera_matrix, projection_matrix, viewport_matrix) {
+    //TODO: this is where I need to change stuff
+    return new Point(point.x, point.y, 0);
+}
+
+var line = function(p1, p2, camera_matrix, projection_matrix, viewport_matrix) {
+    p1 = get2d_coords(p1, camera_matrix, projection_matrix, viewport_matrix);
+    p2 = get2d_coords(p2, camera_matrix, projection_matrix, viewport_matrix);
+
+    //eh, I'll waste a little efficiency for everything to be more sensible and concise
+    ctx.beginPath();
+    ctx.moveTo(p1.x, p1.y);
+    ctx.lineTo(p2.x, p2.y);
+    ctx.stroke();
+}
 
 var x_slider = document.getElementById('x_slider');
 var y_slider = document.getElementById('y_slider');
@@ -15,9 +28,9 @@ var yrot_slider = document.getElementById('yrot_slider');
 var zrot_slider = document.getElementById('zrot_slider');
 
 
-var corner1 = new point(-1, -1, -1);
-var corner2 = new point(1,1,1);
-var cube = new rprism(corner1, corner2);
+var corner1 = new Point(-1, -1, -1);
+var corner2 = new Point(1,1,1);
+var cube = new Rprism(corner1, corner2);
 
 
 
