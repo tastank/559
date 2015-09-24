7583ac46c3b4a66782a38f25e47bc85b5bc00753
Tyler A Stank <tbear2500@gmail.com>
2015-09-24 13:56:06 -0500

    Fixed references to member vars

--- assign3/Rprism.js
+++ assign3/Rprism.js
@@ -5,7 +5,7 @@ var Rprism = function(c1, c2) {
     this.c2 = c2;
 }
 
-Rprism.prototype.set = function(c1,c2) {
+Rprism.prototype.set = function(c1, c2) {
     this.c1 = c1;
     this.c2 = c2;
 }
@@ -13,14 +13,14 @@ Rprism.prototype.set = function(c1,c2) {
 Rprism.prototype.draw = function(camera_matrix, projection_matrix, viewport_matrix) {
     //this will be fun
     //calculate corners
-    var p1 = new Point(c1.x, c1.y, c1.z);
-    var p2 = new Point(c1.x, c1.y, c2.z);
-    var p3 = new Point(c1.x, c2.y, c1.z);
-    var p4 = new Point(c1.x, c2.y, c2.z);
-    var p5 = new Point(c2.x, c1.y, c1.z);
-    var p6 = new Point(c2.x, c1.y, c2.z);
-    var p7 = new Point(c2.x, c2.y, c1.z);
-    var p8 = new Point(c2.x, c2.y, c2.z);
+    var p1 = new Point(this.c1.x, this.c1.y, this.c1.z);
+    var p2 = new Point(this.c1.x, this.c1.y, this.c2.z);
+    var p3 = new Point(this.c1.x, this.c2.y, this.c1.z);
+    var p4 = new Point(this.c1.x, this.c2.y, this.c2.z);
+    var p5 = new Point(this.c2.x, this.c1.y, this.c1.z);
+    var p6 = new Point(this.c2.x, this.c1.y, this.c2.z);
+    var p7 = new Point(this.c2.x, this.c2.y, this.c1.z);
+    var p8 = new Point(this.c2.x, this.c2.y, this.c2.z);
 
     line(p1, p2, camera_matrix, projection_matrix, viewport_matrix); 
     line(p1, p3, camera_matrix, projection_matrix, viewport_matrix); 
